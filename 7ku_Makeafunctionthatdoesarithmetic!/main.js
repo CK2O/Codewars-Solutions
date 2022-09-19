1149b4e3d5e733453778e386b5ae85623c4d9e15
Cormac Kerins <98518657+CK2O@users.noreply.github.com>
2022-09-19 14:35:44 +0200

    Create main.js

---
+++ 7ku_Makeafunctionthatdoesarithmetic!/main.js
@@ -0,0 +1,12 @@
+//Make a function that does arithmetic!
+function arithmetic(a, b, operator){
+    if(operator === "add"){
+      return a + b;
+    } else if(operator === "subtract"){
+      return a - b;
+    } else if(operator === "multiply"){
+      return a * b;
+    } else if(operator === "divide"){
+      return a/b;
+    }
+  }
