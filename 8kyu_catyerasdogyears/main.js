6e456059eca44f10e694b33a6556559718ec96c7
Cormac Kerins <98518657+CK2O@users.noreply.github.com>
2022-09-30 11:31:51 +0200

    Create main.js

---
+++ 8kyu_catyerasdogyears/main.js
@@ -0,0 +1,23 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let catYears=0
+    let dogYears=0
+    
+    if(humanYears>0){
+      for(let i=1;i<=humanYears;i++)
+        if (i==1){
+          catYears+=15
+          dogYears+=15
+        }
+      else if (i==2){
+          catYears+=9
+          dogYears+=9
+        }
+      else if (i>2){
+          catYears+=4
+          dogYears+=5
+        } 
+      }
+    
+    return [humanYears,catYears,dogYears];
+  }
+  
